635ad5a2110a626f2f9a7f5d105c440308409c32
Will Spencer <willspencer16@gmail.com>
2021-05-04 14:47:17 +0100

    Started fourth user story. Updated the deposit method so when a deposit is made, the date of the deposit is saved, along with the amount deposited.

--- lib/account.rb
+++ lib/account.rb
@@ -1,12 +1,15 @@
 class Account
-    attr_reader :balance
+    attr_reader :balance, :history
 
     def initialize
         @balance = 0
+        @history = []
     end
 
     def deposit(amount)
+        date = Time.new
         @balance += amount
+        @history << {:date => date.strftime("%d/%m/%Y"), :amount => amount}
     end
 
     def withdraw(amount)
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,7 +7,6 @@ describe Account do
         end
 
         context 'is changed when' do
-
             it 'user makes a deposit' do
                 subject.deposit(100)
                 expect(subject.balance).to eq(100)
@@ -17,6 +16,16 @@ describe Account do
                 subject.deposit(100)
                 subject.withdraw(50)
                 expect(subject.balance).to eq(50)
+            end
+        end
+    end
+
+    context 'history' do
+        context 'has dates when' do
+            it 'user makes a deposit' do
+                date = Time.new
+                subject.deposit(100)
+                expect(subject.history).to include(:date => date.strftime("%d/%m/%Y"), :amount => 100)
             end
         end
     end
