b018a10d9209e967a1bf5f235d0152354effb23f
Will Spencer <willspencer16@gmail.com>
2021-05-04 13:37:18 +0100

    Finished basic completion of second user story, which includes a deposit method that changes the account balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,14 @@
 require 'account.rb'
 
 describe Account do
-    it 'has a starting balance of zero' do
-        expect(subject.balance).to eq(0)
+    context 'balance' do
+        it 'starts as zero' do
+            expect(subject.balance).to eq(0)
+        end
+
+        it 'can be topped up when user makes a deposit' do
+            subject.deposit(100)
+            expect(subject.balance).to eq(100)
+        end
     end
 end
