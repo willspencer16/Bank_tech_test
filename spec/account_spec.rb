3b07cbf37c41e9ed4e11a4749c4dfeeef11fe700
Will Spencer <willspencer16@gmail.com>
2021-05-04 13:43:42 +0100

    Finished basic completion of third user story, which includes a withdraw method that changes the account balance accordingly

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,9 +6,18 @@ describe Account do
             expect(subject.balance).to eq(0)
         end
 
-        it 'can be topped up when user makes a deposit' do
-            subject.deposit(100)
-            expect(subject.balance).to eq(100)
+        context 'is changed when' do
+
+            it 'user makes a deposit' do
+                subject.deposit(100)
+                expect(subject.balance).to eq(100)
+            end
+
+            it 'user makes a withdrawal' do
+                subject.deposit(100)
+                subject.withdraw(50)
+                expect(subject.balance).to eq(50)
+            end
         end
     end
 end
